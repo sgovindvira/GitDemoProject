3354513ddb4f2ef342bccec508224b4ce5af4835
sgovindvira <112399561+sgovindvira@users.noreply.github.com>
2025-03-16 12:09:03 -0400

    As per conversation I have updated my code.

--- src/github_Project/Test_2_by_tester.java
+++ src/github_Project/Test_2_by_tester.java
@@ -8,6 +8,11 @@ public class Test_2_by_tester {
 	public void main(String[]args)
 	{
 		System.out.print("Hi I am a tester");
+		
+		
+		System.out.println("As per comment. Adding one more line");
 	}
+	
+	
 
 }
